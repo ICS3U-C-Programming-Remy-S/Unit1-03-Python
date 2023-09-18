6b975142e9490737540094247f91aeec7a08b960
Remy Skelton <remy.skelton@stu.ocsb.ca>
2023-09-18 22:00:03 +0000

    Hello, World! but with style

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+# /usr/bin/env python3
+
+# Created By: Remy Skelton
+# Date: Sep 14, 2023
+# This program displays Hello, World! with proper style
+
+
+def main():
+    print("Hello, World with proper sytle")
+    print("I sm testing my function here.")
+
+
+if __name__ == "__main__":
+    main()
